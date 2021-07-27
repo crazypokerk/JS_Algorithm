06a3bbda5b817dc278d08700a7a25d15d7953e92
crazypokerk <downnn@live.com>
2021-07-27 09:21:56 +0800

    reverseListNode, pass

---
+++ 链表/反转链表.js
@@ -0,0 +1,24 @@
+function ListNode(val, next) {
+  this.val = val == undefined ? 0 : val
+  this.next = next == undefined ? 0 : val
+}
+
+/**
+ * 思路：
+ *  三指针，前、当前、后指针
+ */
+
+// @input head
+// @output ListNode
+var reverseListNode = (head) => {
+  if (head == null || head.next == null) return head
+  let front = null,
+    cur = head
+  while (cur) {
+    let next = cur.next
+    cur.next = front
+    front = cur
+    cur = next
+  }
+  return front
+}

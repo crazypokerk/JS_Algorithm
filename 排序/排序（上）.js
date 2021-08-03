edccad63693de20949f99639a89478ff72f86792
crazypokerk <downnn@live.com>
2021-08-03 10:20:18 +0800

    pass, submit

---
+++ 排序/排序（上）.js
@@ -0,0 +1,75 @@
+/**
+ * 冒泡排序
+ *  array.length - i - 1
+ *  精妙之处在与此，减 i 优化已经冒泡了的元素；
+ *               减 1 在于比较元素是当前和下一个。
+ * @param {Array} array 
+ * @returns Array
+ */
+let BubbleSort = (array) => {
+    if (array.length <= 1) return array;
+    for (let i = 0; i < array.length; i++) {
+        let flag = false;
+        for (let j = 0; j < array.length - i - 1; j++) {
+            if (array[j] > array[j + 1]) {
+                let tmp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = tmp;
+                flag = true;
+            }
+        }
+        if (!flag) break;
+    }
+    return array;
+}
+
+var a = BubbleSort([3, 5, 6, 2, 4, 1, 9, 0]);
+console.log(a);
+
+/**
+ * 插入排序
+ *  [已排序区间]，[未排序区间]
+ * [0, i-1(j)], [i, n-1]
+ * @param {Array} array
+ * @returns Array
+ */
+let InsertSort = (array) => {
+    if (array.length <= 1) return array;
+    for (let i = 1; i < array.length; i++) {
+        let tmp = array[i];
+        let j = i - 1;
+        for (; j >= 0; j--) {
+            if (array[j] > tmp) array[j + 1] = array[j];
+            else break;
+        }
+        array[j + 1] = tmp;
+    }
+    return array;
+}
+var b = InsertSort([4, 5, 6, 3, 2, 1]);
+console.log(b);
+
+/**
+ * 选择排序
+ *  [已排序区间]，[未排序区间]
+ * [0, i], [i+1, n-1]
+ * @param {Array} array 
+ * @returns Array
+ */
+let SelectSort = (array) => {
+    if (array.length <= 1) return array;
+    for (let i = 0; i < array.length; i++) {
+        let min = i;
+        for (let j = i + 1; j < array.length; j++) {
+            if (array[j] < array[min]) min = j;
+        }
+        if (min != i) {
+            let tmp = array[min];
+            array[min] = array[i];
+            array[i] = tmp;
+        }
+    }
+    return array;
+}
+var c = SelectSort([4, 5, 6, 3, 2, 1]);
+console.log(b);

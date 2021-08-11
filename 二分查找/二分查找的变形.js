bb8a89c30f1b151cb4906bcc53e0b955a149a639
crazypokerk <downnn@live.com>
2021-08-11 22:04:51 +0800

    pass, submit

--- 二分查找/二分查找的变形.js
+++ 二分查找/二分查找的变形.js
@@ -1,11 +1,14 @@
 /**
- * 二分查找：查找第一个等于val的值
+ * 二分查找：查找第一个等于 val 的值
  * @param {Array} arr 
  * @param {number} val 
  * @returns 
  */
 const FirstEleBinarySearch = (arr, val) => {
     let low = 0, high = arr.length - 1;
+    // 为什么是 low <= high，而不是 low < high ？
+    // 原因是因为当查找的数组个数为偶数时，low 或 high 再倒数第二次比较时就退出 while 循环
+    // 也就无法找到 val
     while (low <= high) {
         let mid = low + ((high - low) >> 1);
         if (arr[mid] < val) {
@@ -22,4 +25,66 @@ const FirstEleBinarySearch = (arr, val) => {
         }
     }
     retrun - 1;
+}
+
+/**
+ * 二分查找：查找最后一个等于 val 的值
+ * @param {Array} arr 
+ * @param {number} val 
+ * @returns 
+ */
+const LastEleBinarySearch = (arr, val) => {
+    let low = 0, high = arr.length - 1;
+    while (low <= high) {
+        let mid = low + ((high - low) >> 1);
+        if (arr[mid] < val) {
+            low = mid + 1;
+        } else if (arr[mid] > val) {
+            high = mid - 1;
+        } else {
+            if (mid == length - 1 || arr[mid + 1] != val) return mid;
+            else low = mid + 1;
+        }
+    }
+    return -1;
+}
+
+/**
+ * 二分查找：查找第一个大于 val 的值
+ * @param {Array} arr 
+ * @param {number} val 
+ */
+const FirstGreaterEleBinarySearch = (arr, val) => {
+    let low = 0, high = arr.length - 1;
+    while (low <= high) {
+        let mid = low + ((high - low) >> 1);
+        while (low <= high) {
+            if (arr[mid] >= val) {
+                if (arr[mid - 1] < val || mid == 0) return mid;
+                else high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+    }
+    return -1;
+}
+
+/**
+ * 二分查找：查找最后一个小于 val 的值
+ * @param {Array} arr 
+ * @param {number} val 
+ */
+const LastSmallerEleBinarySearch = (arr, val) => {
+    let low = 0, high = arr.length - 1;
+    while (low <= high) {
+        let mid = low + ((high - low) >> 1);
+        if (arr[mid] <= val) {
+            if (arr[mid + 1] > val || mid == arr.length - 1) return mid;
+            else low = mid + 1;
+        } else {
+            high = mid - 1;
+        }
+    }
+    return -1;
 }

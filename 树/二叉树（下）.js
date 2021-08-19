1baeb3028b236bc990fb64a3de75262b8d7c2906
crazypokerk <downnn@live.com>
2021-08-19 21:30:04 +0800

    BST delete pass, submit

--- 树/二叉树（下）.js
+++ 树/二叉树（下）.js
@@ -3,6 +3,19 @@ function TreeNode(val, left, right) {
     this.left = (left === undefined ? null : left);
     this.right = (right === undefined ? null : right);
 }
+
+function initTree() {
+    const i = new TreeNode(29, null, null);
+    const h = new TreeNode(31, null, null);
+    const g = new TreeNode(19, null, i);
+    const f = new TreeNode(26, g, h);
+    const e = new TreeNode(15, null, null);
+    const d = new TreeNode(5, null, null);
+    const c = new TreeNode(16, e, f);
+    const b = new TreeNode(9, d, null);
+    const a = new TreeNode(14, b, c);
+    return root = a;
+}
 /**
  * 二分查找树 --- 查找元素
  * @param {number} data 
@@ -60,20 +73,38 @@ const BST_delete = (delData, root) => {
         if (delData > cur.val) cur = cur.right;
         else cur = cur.left;
     }
-    if (!cur) return false;
-    // 1、无子节点或一个子节点
+    if (!cur) return;
+
+    // 此时 prev 指向 cur 的父节点
+    // Step 1、两个子节点
+    if (cur.left && cur.right) {
+        let tmpCur = cur.right, tmpPrev = tmpCur;
+        while (tmpCur.left) {
+            tmpPrev = tmpCur;
+            tmpCur = tmpCur.left;
+        }
+        cur.val = tmpCur.val;
+        // 这里是为了将两个子节点问题转化为一个子节点或无子节点的问题
+        // 因为到这里，tmpCur 指向的子树一定只有右子节点或无节点
+        cur = tmpCur;
+        prev = tmpPrev;
+    }
+
+    // Step 2、无子节点或一个子节点
+    // 这里真的是太巧秒了，一步到位，直接将无子节点情况和一个子节点情况都囊括到
     let child;
     if (cur.left) child = cur.left;
     else if (cur.right) child = cur.right;
     else child = null;
-    prev = child;
 
     if (!prev) root = child;
-    else if (prev.left) prev.left = child;
+    else if (prev.left == cur) prev.left = child;
     else prev.right = child;
-    // 2、两个子节点
-
 }
+var root = initTree();
+console.log(root);
+BST_delete(16, root);
+console.log(root);
 
 const BST_search_max_data = root => {
 

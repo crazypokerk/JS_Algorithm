f9dbb99c83326411883a4451904190a6d9c79e39
crazypokerk <downnn@live.com>
2021-09-01 09:49:16 +0800

    LC 724 pass

--- 数组/寻找数组的中心索引.js
+++ 数组/寻找数组的中心索引.js
@@ -1,8 +1,19 @@
 /**
- * 
+ * Leetcode:724
  * @param {Array} nums 
  * @returns {number}
  */
 const pivotIndex = nums => {
-
+    let prevSum = [0], leftSum, rightSum;
+    for (let i = 0; i < nums.length; i++) {
+        prevSum[i + 1] = nums[i] + prevSum[i];
+    } 1
+    for (let i = 0; i < nums.length; i++) {
+        leftSum = prevSum[i];
+        rightSum = prevSum[nums.length] - prevSum[i] - nums[i];
+        if (leftSum === rightSum) return i;
+    }
+    return -1;
 }
+let nums = [1, 7, 3, 6, 5, 6];
+console.log(pivotIndex(nums));

2bfb94ffb535bfc3da91f0ecce7e8462207b312d
crazypokerk <downnn@live.com>
2021-09-06 12:08:58 +0800

    addSrtings pass, submit

--- 字符串/字符串相加.js
+++ 字符串/字符串相加.js
@@ -8,14 +8,17 @@ const addStrings = (num1, num2) => {
     if (!num1) return num2;
     if (!num2) return num1;
 
-    let i = n1Arr.length, j = n2Arr.length, res = [], add = 0;
+    let i = num1.length - 1, j = num2.length - 1, add = 0;
+    const res = [];
     while (i >= 0 || j >= 0 || add != 0) {
-        const x = i >= 0 ? num1.charAt(i) - '0' : 0;
-        const y = j >= 0 ? num2.charAt(j) - '0' : 0;
-        const sum = i + j + add;
-        res.push(sum % 10);
-        add = Math.floor(sum / 10);
-        i--, j--;
+        let a = i < 0 ? 0 : num1.charAt(i) - '0';
+        let b = j < 0 ? 0 : num2.charAt(j) - '0';
+        res.push((a + b + add) % 10);
+        add = Math.floor((a + b + add) / 10);
+        i--;
+        j--;
     }
-    return [...res.reverse()];
+    return res.reverse().join('');
 }
+let num1 = '101', num2 = '8098';
+console.log(addStrings(num1, num2));

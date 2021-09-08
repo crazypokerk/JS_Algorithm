c7cd55cdfbfee67cf98492c4af1d6c340f3dfa2f
crazypokerk <downnn@live.com>
2021-09-09 07:40:18 +0800

    addBinary pass, submit

--- 字符串/easy/二进制求和.js
+++ 字符串/easy/二进制求和.js
@@ -5,5 +5,27 @@
  * @return {string}
  */
 var addBinary = function (a, b) {
-
+    let i = a.length - 1, j = b.length - 1, add = 0;
+    const res = [];
+    while (i >= 0 && j >= 0) {
+        let x = a[i--] - '0';
+        let y = b[j--] - '0';
+        let sum = x + y + add;
+        add = Math.floor(sum / 2);
+        res.push(sum % 2);
+    }
+    while (i >= 0) {
+        let sum = add + (a[i--] - '0');
+        add = Math.floor(sum / 2);
+        res.push(sum % 2);
+    }
+    while (j >= 0) {
+        let sum = add + (b[j--] - '0');
+        add = Math.floor(sum / 2);
+        res.push(sum % 2);
+    }
+    if (add == 1) res.push(add);
+    return res.reverse().join('');
 };
+let a = '11', b = '1';
+console.log(addBinary(a, b));

f09ee7edac9c719c9c0787f05d44a07b78fc60cf
crazypokerk <downnn@live.com>
2021-09-08 14:05:49 +0800

    reverseString pass, submit

--- 字符串/easy/反转字符串.js
+++ 字符串/easy/反转字符串.js
@@ -3,6 +3,14 @@
  * @param {character[]} s
  * @return {void} Do not return anything, modify s in-place instead.
  */
-var reverseString = function (s) {
-
+var reverseString = s => {
+    if (s.length <= 1) return;
+    let i = 0, j = s.length - 1;
+    while (i < j) {
+        [s[i++], s[j--]] = [s[j], s[i]];
+    }
 };
+
+let input = ["h", "e", "l", "l"];
+reverseString(input);
+console.log(input);

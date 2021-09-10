3850ada55f4473e18f0d1355a9ba6f14a52423c2
crazypokerk <downnn@live.com>
2021-09-10 17:28:50 +0800

    add bug_record.js

---
+++ bug_record.js
@@ -0,0 +1,37 @@
+// 数组取值用 '[]'，调用方法用 '()'
+let array = ['a', 'b', 'c'];
+array[1];
+let obj = { 'a': 1, 'b': 2 };
+obj.a;
+obj['a'];
+array.push();
+
+// 无论条件是否满足都会执行 i++
+if (a[i++] > 0) { } else { }
+
+// 等于和严格等于
+console.log(a == b);
+console.log(a === b);
+// 普通相等 == 不能区分 0 和 false
+// 0 -> (number) false => (boolean)
+console.log(0 == false);  // true
+// '' -> (string)
+// undefined 用于未定义的值 —— 只有一个 undefined 值的独立类型
+// 八大将： number-bright-string-boolean-null-undefined-object-symbol
+// 普通相等 == 也不能区分空字符 '' 和 false
+console.log('' == false); // true
+// 严格等于不会进行类型转换
+console.log(undefined == null); // true
+console.log(undefined === null); // false
+
+// 当使用数学表达式或其他比较方法时 ： < > <= >=
+// null/undefined 会被转化为数字 null -> 0  undefined -> NaN(Not a Number)
+// 双等于 == 和 普通比较符 < > <= >= 代码逻辑相互独立
+console.log(null >= 0); // true
+console.log(null <= 0); // true
+console.log(null > 0);  // false
+console.log(null > 0);  // false
+
+if (val) { }   // 不为空执行
+if (!val) { }  // 为空执行
+// 判空和判不空

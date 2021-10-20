1f101a6dc6650418ccd4af07a616e1df367e1276
crazypokerk <downnn@live.com>
2021-10-20 22:15:47 +0800

    deleteDuplicates pass, submit

---
+++ 链表/easy/7-删除排序链表中的重复元素.js
@@ -0,0 +1,14 @@
+/**
+ * Leetcode:83
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var deleteDuplicates = function (head) {
+    if (!head) return head;
+    let cur = head;
+    while (cur.next) {
+        if (cur.val === cur.next.val) cur.next = cur.next.next;
+        else cur = cur.next;
+    }
+    return head;
+};

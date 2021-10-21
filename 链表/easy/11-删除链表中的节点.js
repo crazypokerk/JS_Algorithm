542b112fec3fc3d5be85bd09fbf6d6af50294a39
crazypokerk <downnn@live.com>
2021-10-21 22:32:07 +0800

    237deleteNode pass, submit

---
+++ 链表/easy/11-删除链表中的节点.js
@@ -0,0 +1,9 @@
+/**
+ * Leetcode:237
+ * @param {ListNode} node
+ * @return {void} Do not return anything, modify node in-place instead.
+ */
+var deleteNode = function (node) {
+    node.val = node.next.val;
+    node.next = node.next.next;
+};

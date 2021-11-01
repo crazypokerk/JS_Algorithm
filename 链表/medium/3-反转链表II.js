47f351628e0eceb46f62e319d2f4ea74a9e5bdb3
crazypokerk <downnn@live.com>
2021-11-01 21:44:01 +0800

    reverseBetween (simple method pass)

---
+++ 链表/medium/3-反转链表II.js
@@ -0,0 +1,32 @@
+/**
+ * Leetcode:92
+ * @param {ListNode} head
+ * @param {number} left
+ * @param {number} right
+ * @return {ListNode}
+ */
+var reverseBetween = function (head, left, right) {
+    if (!head) return head;
+    const tmp = new ListNode(-1);
+    tmp.next = head;
+    let pre = tmp;
+    for (let i = 0; i < left - 1; i++) pre = pre.next;
+    let back = pre;
+    for (let i = 0; i < right - left + 1; i++) back = back.next;
+
+    let l = pre.next, r = back.next;
+    pre.next = null;
+    back.next = null;
+    (head => {
+        let pre = null, cur = head;
+        while (cur) {
+            let next = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = next;
+        }
+    })(l);
+    pre.next = back;
+    l.next = r;
+    return tmp.next;
+};

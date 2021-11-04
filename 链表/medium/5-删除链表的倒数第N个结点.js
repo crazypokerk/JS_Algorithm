81428a145bb29d7c8cd400817eb108b13153562b
crazypokerk <downnn@live.com>
2021-11-04 20:25:29 +0800

    removeNthFromEnd1 (双指针) pass, submit

--- 链表/medium/5-删除链表的倒数第N个结点.js
+++ 链表/medium/5-删除链表的倒数第N个结点.js
@@ -22,3 +22,20 @@ var removeNthFromEnd = function (head, n) {
 function peek(stack) {
     return stack[stack.length - 1];
 }
+
+// 双指针
+var removeNthFromEnd1 = function (head, n) {
+    if (!head) return head;
+    const sub = new ListNode(-1);
+    sub.next = head;
+    let first = sub, second = sub;
+    for (let i = 0; i < n; i++) {
+        first = first.next;
+    }
+    while (first.next) {
+        first = first.next;
+        second = second.next;
+    }
+    second.next = second.next.next;
+    return sub.next;
+}

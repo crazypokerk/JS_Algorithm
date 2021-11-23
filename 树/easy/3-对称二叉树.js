db0756c4d3146e91b53f88f708bee28594826984
crazypokerk <downnn@live.com>
2021-11-23 21:17:21 +0800

    isSymmetric1(bfs) pass, submit

--- 树/easy/3-对称二叉树.js
+++ 树/easy/3-对称二叉树.js
@@ -12,3 +12,22 @@ var dfs = (left, right) => {
     if (left.val !== right.val) return false;
     return dfs(left.left, right.right) && dfs(left.right, right.left);
 }
+
+var isSymmetric1 = root => {
+    if (!root || (!root.left && !root.right)) return true;
+    const queue = [];
+    queue.push(root.left);
+    queue.push(root.right);
+    while (queue.length !== 0) {
+        let l = queue.shift();
+        let r = queue.shift();
+        if (!l && !r) continue;
+        if (!l || !r) return false;
+        if (l.val !== r.val) return false;
+        queue.push(l.left);
+        queue.push(r.right);
+        queue.push(l.right);
+        queue.push(r.left);
+    }
+    return queue.length === 0;
+}

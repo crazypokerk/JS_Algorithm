7711ad47800a901a1450e90ee8a3421b0c6ae830
crazypokerk <downnn@live.com>
2021-11-28 21:40:24 +0800

    postorderTraversal1(iteration) pass, submit

--- 树/easy/4-二叉树的后序遍历.js
+++ 树/easy/4-二叉树的后序遍历.js
@@ -15,3 +15,32 @@ var dfs = (root, res) => {
     dfs(root.right, res);
 }
 
+// var peek = function () {
+//     return this[this.length - 1];
+// }
+// Array.prototype.peek = peek;
+var peek = stack => {
+    return stack[stack.length - 1];
+}
+// 迭代
+var postorderTraversal1 = root => {
+    const stack = [], result = [];
+    if (!root) return result;
+    let cur = root, tmp = null;
+    while (stack.length > 0 || cur) {
+        while (cur) {
+            stack.push(cur);
+            cur = cur.left;
+        }
+        cur = stack.pop();
+        if (!cur.right || cur.right == tmp) {
+            result.push(cur.val);
+            tmp = cur;
+            cur = null;
+        } else {
+            stack.push(cur);
+            cur = cur.right;
+        }
+    }
+    return result;
+}

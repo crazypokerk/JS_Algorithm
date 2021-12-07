e741af88f032be1aa3a7f1a831f987136478dac5
crazypokerk <downnn@live.com>
2021-12-07 21:48:30 +0800

    isSameTree pass(DFS), submit

---
+++ 树/easy/8-相同的树.js
@@ -0,0 +1,12 @@
+/**
+ * Leetcode:100
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {boolean}
+ */
+var isSameTree = function (p, q) {
+    if (!p && !q) return true;
+    if (!p || !q) return false;
+    if (p.val !== q.val) return false;
+    return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+};

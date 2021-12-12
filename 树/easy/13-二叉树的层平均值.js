bcf899c2e86a23576bdbae00d1f4adc5fdc429df
crazypokerk <downnn@live.com>
2021-12-12 21:47:26 +0800

    averageOfLevels(DFS) pass, submit

---
+++ 树/easy/13-二叉树的层平均值.js
@@ -0,0 +1,27 @@
+/**
+ * Leetcode:637
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var averageOfLevels = function (root) {
+    const sums = [], counts = [];
+    const result = [];
+    if (!root) return result;
+    let dfs = (root, lev) => {
+        if (!root) return 0;
+        if (lev < sums.length) {
+            sums[lev] += root.val;
+            counts[lev] += 1;
+        } else {
+            sums.push(root.val);
+            counts.push(1);
+        }
+        dfs(root.left, lev + 1);
+        dfs(root.right, lev + 1);
+    }
+    dfs(root, 0);
+    for (let i = 0; i < sums.length; i++) {
+        result.push(sums[i] / counts[i]);
+    }
+    return result;
+};

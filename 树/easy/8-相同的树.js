8db2201726d5f6d5976c9270247580b0f05aedfa
crazypokerk <downnn@live.com>
2021-12-24 12:51:52 +0800

    isSmaeTree(BFS) pass, submit

--- 树/easy/8-相同的树.js
+++ 树/easy/8-相同的树.js
@@ -10,3 +10,19 @@ var isSameTree = function (p, q) {
     if (p.val !== q.val) return false;
     return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
 };
+var isSmaeTreeBFS = (p, q) => {
+    if (!p && !q) return true;
+    if (!p || !q) return false;
+    const queue = [p, q];
+    while (queue.length) {
+        let p = queue.shift();
+        let q = queue.shift();
+        if (!p && !q) continue;
+        if (!p || !q || p.val != q.val) return false;
+        queue.push(p.left);
+        queue.push(q.left);
+        queue.push(p.right);
+        queue.push(q.right);
+    }
+    return true;
+}

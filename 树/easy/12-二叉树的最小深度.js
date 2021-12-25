558a0dd8591631d9926d9d85d8edeece212ba830
crazypokerk <downnn@live.com>
2021-12-25 17:11:43 +0800

    minDepth(BFS) pass, submit

--- 树/easy/12-二叉树的最小深度.js
+++ 树/easy/12-二叉树的最小深度.js
@@ -13,3 +13,19 @@ var minDepth = function (root) {
         return Math.min(l, r) + 1;
     }
 }
+var minDepthBFS = root => {
+    if (!root) return 0;
+    const queue = [root];
+    let minD = 1;
+    while (queue.length) {
+        let size = queue.length;
+        for (let i = 0; i < size; i++) {
+            let cur = queue.shift();
+            if (!cur.left && !cur.right) return minD;
+            if (cur.left) queue.push(cur.left);
+            if (cur.right) queue.push(cur.right);
+        }
+        minD++;
+    }
+    return minD;
+}

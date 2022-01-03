2feae19b9d5f8ee6017585cd18a6c19743956b70
crazypokerk <downnn@live.com>
2022-01-03 16:35:53 +0800

    minCostClimbingStairs(DP) pass, submit

--- 动态规划/1.基础题目/3-使用最小花费爬楼梯.js
+++ 动态规划/1.基础题目/3-使用最小花费爬楼梯.js
@@ -4,5 +4,12 @@
  * @return {number}
  */
 var minCostClimbingStairs = function (cost) {
-    
-};
+    const len = cost.length;
+    const dp = new Array(len);
+    dp[0] = cost[0];
+    dp[1] = cost[1];
+    for (let i = 2; i < len; i++) {
+        dp[i] = Math.min(dp[i - 1], dp[i - 2]) + cost[i];
+    }
+    return Math.min(dp[len - 1], dp[len - 2]);
+}

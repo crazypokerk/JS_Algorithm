52f8aafa014f0eb3040e53f8635f8c7c35de1a29
crazypokerk <downnn@live.com>
2022-01-05 20:34:22 +0800

    numTrees(DP) pass, submit

---
+++ 动态规划/1.基础题目/7.不同的二叉搜索树.js
@@ -0,0 +1,15 @@
+/**
+ * Leetcode:96
+ * @param {number} n
+ * @return {number}
+ */
+var numTrees = function (n) {
+    const dp = Array(n + 1).fill(0);
+    dp[0] = 1;
+    for (let i = 1; i <= n; i++) {
+        for (let j = 1; j <= i; j++) {
+            dp[i] += dp[j - 1] * dp[i - j];
+        }
+    }
+    return dp[n];
+};

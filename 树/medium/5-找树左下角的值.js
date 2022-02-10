050e076615ac4ac05dbaf0ec0928638556d039d3
crazypokerk <downnn@live.com>
2022-02-10 17:29:10 +0800

    findBottomLeftValue(BFS) pass, submit

--- 树/medium/5-找树左下角的值.js
+++ 树/medium/5-找树左下角的值.js
@@ -12,7 +12,18 @@
  * @return {number}
  */
 var findBottomLeftValue = function (root) {
-
+    const queue = [root];
+    let value = 0;
+    while (queue.length) {
+        let len = queue.length;
+        for (let i = 0; i < len; i++) {
+            const cur = queue.shift();
+            if (i == 0) value = cur.val;
+            if (cur.left) queue.push(cur.left);
+            if (cur.right) queue.push(cur.right);
+        }
+    }
+    return value;
 };
 var findBottomLeftValueDFS = root => {
     let maxDepth = Number.NEGATIVE_INFINITY;

bbd1cf6a3d6d0896181bfdca0907b127f0817b42
crazypokerk <downnn@live.com>
2022-03-02 16:35:53 +0800

    lowestCommonAncestor(DFS) pass, submit

---
+++ 树/medium/11.二叉树的最近公共祖先.js
@@ -0,0 +1,15 @@
+/**
+ * Leetcode:236
+ * @param {TreeNode} root
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {TreeNode}
+ */
+var lowestCommonAncestor = function (root, p, q) {
+    if (p == root || q == root || !root) return root;
+    let left = lowestCommonAncestor(root.left, p, q);
+    let right = lowestCommonAncestor(root.right, p, q);
+    if (left && right) return root;
+    if (!left) return right;
+    else return left;
+};
